46aca2e9eb102776ca4a96e8d409018d2ace2cda
Mironov Ury <mironoff2007@rambler.ru>
2023-06-18 11:19:34 +0300

    Initial commit

---
+++ desktop/build.gradle.kts
@@ -0,0 +1,41 @@
+
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "ru.mironov"
+version = "1.0-SNAPSHOT"
+
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(":shared"))
+                implementation(compose.desktop.currentOs)
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Logistics"
+            packageVersion = "1.0.0"
+        }
+    }
+}
+
---
+++ shared/src/desktopMain/kotlin/main.desktop.kt
@@ -0,0 +1,7 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+
+@Composable
+fun MainView() = App()
