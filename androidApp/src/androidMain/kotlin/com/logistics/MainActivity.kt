c407654065f01a96fb8f3c042707fe80b1781804
Mironov Ury <mironoff2007@rambler.ru>
2023-06-18 13:50:09 +0300

    rename app

--- androidApp/src/androidMain/kotlin/com/logistics/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/logistics/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.myapplication
+package com.logistics
 
 import MainView
 import android.os.Bundle
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "MyApplication"
+rootProject.name = "Logistics"
 
 include(":androidApp")
 include(":shared")
