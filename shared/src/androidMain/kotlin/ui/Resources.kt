2d4a911d1c65b668ac2b38646d8f1db499b96b0f
mac <mironoff2007@rambler.ru>
2023-06-22 22:16:18 +0300

    refactor drawable res

--- shared/src/androidMain/kotlin/ui/Resources.kt
+++ shared/src/androidMain/kotlin/ui/Resources.kt
@@ -7,23 +7,6 @@ import androidx.compose.ui.res.painterResource
 import ru.mironov.common.res.ImageRes
 
 @Composable
-actual fun getPainterResource(imageRes: ImageRes): Painter {
-    val id = when (imageRes) {
-        /*ImageRes.Box -> R.drawable.box
-        ImageRes.RegisterParcel -> R.drawable.register_parcel
-        ImageRes.Warehouse -> R.drawable.warehouse
-        ImageRes.Settings -> R.drawable.settings
-        ImageRes.Logout -> R.drawable.logout
-        ImageRes.Back -> R.drawable.back_arrow
-        ImageRes.Search -> R.drawable.search
-        ImageRes.Filter -> R.drawable.filter
-        ImageRes.Eye -> R.drawable.eye*/
-        else -> 0//R.drawable.none
-    }
-    return painterResource(id)
-}
-
-@Composable
 actual fun getLocale(): String {
     val context = LocalContext.current
     return context.resources.configuration.locales.get(0).language
--- shared/src/iosMain/kotlin/ui/Resources.kt
+++ shared/src/iosMain/kotlin/ui/Resources.kt
@@ -6,24 +6,6 @@ import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import ru.mironov.common.res.ImageRes
 
-@OptIn(ExperimentalResourceApi::class)
-@Composable
-actual fun getPainterResource(imageRes: ImageRes): Painter {
-    val id = when(imageRes) {
-        ImageRes.Box -> "box.xml"
-        ImageRes.RegisterParcel -> "register_parcel.xml"
-        ImageRes.Warehouse -> "warehouse.xml"
-        ImageRes.Settings -> "settings.xml"
-        ImageRes.Logout -> "logout.xml"
-        ImageRes.Back -> "back_arrow.xml"
-        ImageRes.Search -> "search.xml"
-        ImageRes.Filter -> "filter.xml"
-        ImageRes.Eye -> "eye.xml"
-        else -> "none.xml"
-    }
-    return painterResource("drawable/$id")
-}
-
 @Composable
 actual fun getLocale(): String {
     return "en"
