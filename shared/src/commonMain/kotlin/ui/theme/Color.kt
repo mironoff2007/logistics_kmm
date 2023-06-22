d447971dbf6946f07ce840d75c105b316d6168d1
Mironov Ury <mironoff2007@rambler.ru>
2023-06-22 10:44:52 +0300

    add theme

---
+++ shared/src/commonMain/kotlin/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package ru.mironov.logistics.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple700 = Color(0xFF3700B3)
+val Main = Color(40, 195, 144, 255)
+val MainDark = Color(1, 227, 232)
+val LightBlack = Color(46, 46, 46, 255)
---
+++ shared/src/commonMain/kotlin/ui/theme/Theme.kt
@@ -0,0 +1,46 @@
+package ru.mironov.logistics.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = MainDark,
+    primaryVariant = Purple700,
+    secondary = Color.Blue,
+    surface = LightBlack,
+    background = Color.Black,
+    onPrimary = Color.Black,
+    onSecondary = Color.LightGray,
+    onSurface = Color.LightGray,
+    onBackground = Color.LightGray
+)
+
+private val LightColorPalette = lightColors(
+    primary = Main,
+    primaryVariant = Main,
+    secondary = Main,
+    surface = Color.White,
+    background = Color.LightGray,
+    onPrimary = Color.White,
+    onBackground = Color.Black
+)
+
+@Composable
+fun LogisticsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
