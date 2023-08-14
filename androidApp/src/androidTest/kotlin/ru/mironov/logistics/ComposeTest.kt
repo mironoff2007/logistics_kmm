ada171615792ac205e800b4c14484a1391d5d9d4
Mironov Ury <mironoff2007@rambler.ru>
2023-08-14 11:45:46 +0300

    add assert

--- androidApp/src/androidTest/kotlin/ru/mironov/logistics/ComposeTest.kt
+++ androidApp/src/androidTest/kotlin/ru/mironov/logistics/ComposeTest.kt
@@ -1,11 +1,14 @@
 package ru.mironov.logistics
 
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.mironov.di.ApplicationComponent
+import com.mironov.localization.StringRes
 import kotlinx.datetime.Clock
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -14,6 +17,8 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import ru.mironov.common.ktor.auth.AuthResponse
+import ru.mironov.common.navigation.TopBar
+import ru.mironov.common.res.localizedString
 import ru.mironov.domain.model.auth.TokenResp
 
 @RunWith(AndroidJUnit4::class)
@@ -40,11 +45,13 @@ class ComposeTest {
                 .onAllNodesWithTag(ru.mironov.logistics.ui.screens.login.LOGIN_BTN_TAG)
                 .fetchSemanticsNodes().size == 1
         }
-        composeTestRule.onNodeWithTag(ru.mironov.logistics.ui.screens.login.LOGIN_BTN_TAG).performClick()
-        //composeTestRule.waitUntil(100_000) { false }
+        composeTestRule
+            .onNodeWithTag(ru.mironov.logistics.ui.screens.login.LOGIN_BTN_TAG)
+            .performClick()
 
-
-        assertEquals(true, true)
+        composeTestRule
+            .onNodeWithTag(TopBar.TITLE_TAG)
+            .assert(hasText(localizedString(StringRes.RegisterParcel)))
     }
 
 }
--- ui/src/commonMain/kotlin/ru/mironov/logistics/ui/navigation/TopBar.kt
+++ ui/src/commonMain/kotlin/ru/mironov/logistics/ui/navigation/TopBar.kt
@@ -5,16 +5,31 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.testTag
+import ru.mironov.common.navigation.TopBar.TITLE_TAG
+import ru.mironov.logistics.ui.screens.login.LOGIN_BTN_TAG
 
-@Composable
-fun TopBar(title: String = "", buttonIcon: ImageVector, onButtonClicked: () -> Unit) {
-    TopAppBar(
-        title = { Text(text = title) },
-        navigationIcon = {
-            IconButton(onClick = { onButtonClicked() } ) {
-                Icon(buttonIcon, contentDescription = "")
-            }
-        }
-    )
+object TopBar {
+    const val TITLE_TAG = "title"
 }
+    @Composable
+    fun TopBar(title: String = "", buttonIcon: ImageVector, onButtonClicked: () -> Unit) {
+        TopAppBar(
+            title = {
+                Text(
+                    modifier = Modifier.testTag(TITLE_TAG),
+                    text = title
+                )
+            },
+            navigationIcon = {
+                IconButton(onClick = { onButtonClicked() }) {
+                    Icon(buttonIcon, contentDescription = "")
+                }
+            }
+        )
+    }
+
+
+
