fe8e410ff8c49453f7a7f76db2d5e2c8de55bb73
Mironov Ury <mironoff2007@rambler.ru>
2023-08-15 09:56:19 +0300

    fix test

--- androidApp/src/androidTest/kotlin/ru/mironov/logistics/ComposeTest.kt
+++ androidApp/src/androidTest/kotlin/ru/mironov/logistics/ComposeTest.kt
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith
 import ru.mironov.common.ktor.auth.AuthResponse
 import ru.mironov.common.navigation.TopBar
 import ru.mironov.common.res.localizedString
+import ru.mironov.domain.model.City
 import ru.mironov.domain.model.auth.TokenResp
 import ru.mironov.domain.settings.CommonSettings
 import ru.mironov.domain.settings.UserData
@@ -44,10 +45,14 @@ class ComposeTest {
         clearMemory()
         val expireAt = Clock.System.now().toEpochMilliseconds() + 1000 * 360
         val token = TokenResp(token = "", expireAt = expireAt)
-        val resp = Json.encodeToString(AuthResponse(token))
+        val cities = listOf(City(1, "City1"))
+        val respAuth = Json.encodeToString(AuthResponse(token))
+        val citiesResp = Json.encodeToString(cities)
 
         val ktor = ApplicationComponent.getKtor()
-        ktor.addNextResponse(resp)
+
+        ktor.addNextResponse(citiesResp)
+        ktor.addNextResponse(respAuth)
 
         composeTestRule.setContent {
             MainScreen()
